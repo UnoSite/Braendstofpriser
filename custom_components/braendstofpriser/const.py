67c2e82bef65263a2167cf63743cedca04b17aca
Dennis M <68971788+UnoSite@users.noreply.github.com>
2025-03-07 20:28:29 +0100

    Update const.py

--- custom_components/braendstofpriser/const.py
+++ custom_components/braendstofpriser/const.py
@@ -1,1 +1,19 @@
+DOMAIN = "braendstofpriser"
 
+CONF_COMPANIES = "companies"
+CONF_PRODUCTS = "products"
+
+API_URL = "https://raw.githubusercontent.com/UnoSite/Braendstofpriser/refs/heads/main/data/listprices.json"
+
+PRODUCTS = {
+    "blyfri_92": "Benzin 92 (E5)",
+    "blyfri_95_e10": "Benzin 95 (E10)",
+    "blyfri_95_plus_e10": "Benzin 95 + (E10)",
+    "blyfri_plus_e5": "Benzin 100 (E5)",
+    "diesel_b7": "Diesel (B7)",
+    "diesel_plus": "Diesel +",
+    "hvo_xtl": "HVO (XTL)",
+    "el_normal": "EL (Normal)",
+    "el_hurtig": "EL (Hurtig)",
+    "el_lyn": "EL (Turbo)"
+}
