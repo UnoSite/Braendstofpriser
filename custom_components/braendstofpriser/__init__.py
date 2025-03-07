cefce244442ae6042c0be43758bb67e969e7a991
Dennis M <68971788+UnoSite@users.noreply.github.com>
2025-03-07 20:31:08 +0100

    Update __init__.py

--- custom_components/braendstofpriser/__init__.py
+++ custom_components/braendstofpriser/__init__.py
@@ -1,1 +1,14 @@
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from .const import DOMAIN
 
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return True
