f2560405af3897013a217ea39cad2344e7d66e85
Dennis M <68971788+UnoSite@users.noreply.github.com>
2025-05-27 22:16:05 +0200

    Update config_flow.py

--- custom_components/braendstofpriser/config_flow.py
+++ custom_components/braendstofpriser/config_flow.py
@@ -1,152 +1,68 @@
 import logging
 import voluptuous as vol
 import requests
+
 from homeassistant import config_entries
-from homeassistant.core import callback, HomeAssistant
+from homeassistant.core import callback
 from homeassistant.helpers import selector
-from .const import *
-from .sensor import remove_unused_entities_and_devices  # Importer oprydningsfunktionen
+
+from .const import DOMAIN, API_URL, CONF_PROVIDERS, CONF_PRICE_TYPES
 
 _LOGGER = logging.getLogger(__name__)
 
-def fetch_companies():
-    """Henter listen af selskaber fra brændstofpris-API'et."""
-    _LOGGER.info("Henter liste over selskaber fra API: %s", API_URL)
+def fetch_api_data():
     try:
         response = requests.get(API_URL, timeout=10)
         response.raise_for_status()
-        data = response.json()
-
-        companies = sorted(set(entry.get('selskab') for entry in data.get('priser', []) if entry.get('selskab')))
-
-        if not companies:
-            _LOGGER.warning("API returnerede ingen selskaber.")
-        else:
-            _LOGGER.debug("Modtaget %d selskaber fra API.", len(companies))
-
-        return {company: company for company in companies}
-
-    except (requests.Timeout, requests.RequestException, ValueError) as err:
-        _LOGGER.error("Fejl ved hentning af selskaber: %s", err)
+        return response.json()
+    except Exception as err:
+        _LOGGER.error("Fejl ved API-kald: %s", err)
         return {}
 
-class BraendstofpriserConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Håndterer opsætningen af Brændstofpriser integrationen."""
-
+class BraendstofpriserV3ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Trin 1: Vælg selskaber."""
         if user_input is not None:
-            _LOGGER.debug("Brugeren har valgt selskaber: %s", user_input[CONF_COMPANIES])
-            self.context["selected_companies"] = user_input[CONF_COMPANIES]
-            return await self.async_step_products()
+            self.context["providers"] = user_input[CONF_PROVIDERS]
+            return await self.async_step_price_types()
 
-        companies = await self.hass.async_add_executor_job(fetch_companies)
-        if not companies:
-            _LOGGER.error("Kunne ikke hente selskaber fra API. Afbryder opsætning.")
-            return self.async_abort(reason="cannot_connect")
+        data = await self.hass.async_add_executor_job(fetch_api_data)
+        providers = sorted(set(entry["provider"] for entry in data.values()))
 
         schema = vol.Schema({
-            vol.Required(CONF_COMPANIES, default=[]): selector.SelectSelector(
+            vol.Required(CONF_PROVIDERS): selector.SelectSelector(
                 selector.SelectSelectorConfig(
-                    options=list(companies.keys()),
+                    options=providers,
                     multiple=True,
                     mode=selector.SelectSelectorMode.LIST,
                 )
-            ),
+            )
         })
 
         return self.async_show_form(step_id="user", data_schema=schema)
 
-    async def async_step_products(self, user_input=None):
-        """Trin 2: Vælg brændstofprodukter."""
+    async def async_step_price_types(self, user_input=None):
         if user_input is not None:
-            _LOGGER.debug("Brugeren har valgt produkter: %s", user_input[CONF_PRODUCTS])
-            return self.async_create_entry(title="Brændstofpriser", data={
-                CONF_COMPANIES: self.context["selected_companies"],
-                CONF_PRODUCTS: [PRODUCT_NAME_MAP[name] for name in user_input[CONF_PRODUCTS]]
-            })
+            return self.async_create_entry(
+                title="Brændstofpriser v3",
+                data={
+                    CONF_PROVIDERS: self.context["providers"],
+                    CONF_PRICE_TYPES: user_input[CONF_PRICE_TYPES]
+                }
+            )
+
+        data = await self.hass.async_add_executor_job(fetch_api_data)
+        price_types = sorted(set(entry["price_type"] for entry in data.values()))
 
         schema = vol.Schema({
-            vol.Required(CONF_PRODUCTS, default=[]): selector.SelectSelector(
+            vol.Required(CONF_PRICE_TYPES): selector.SelectSelector(
                 selector.SelectSelectorConfig(
-                    options=list(PRODUCT_NAME_MAP.keys()),
+                    options=price_types,
                     multiple=True,
                     mode=selector.SelectSelectorMode.LIST,
                 )
-            ),
+            )
         })
 
-        return self.async_show_form(step_id="products", data_schema=schema)
-
-class BraendstofpriserOptionsFlowHandler(config_entries.OptionsFlowWithConfigEntry):
-    """Håndterer ændringer af indstillinger efter opsætning."""
-
-    async def async_step_init(self, user_input=None):
-        """Starter reconfiguration-processen."""
-        return await self.async_step_select_companies()
-
-    async def async_step_select_companies(self, user_input=None):
-        """Trin 1: Opdater valgte selskaber."""
-        _LOGGER.info("Reconfiguration: Trin 1 - Vælg selskaber")
-
-        if user_input is not None:
-            _LOGGER.debug("Opdaterede selskaber: %s", user_input[CONF_COMPANIES])
-            self.hass.config_entries.async_update_entry(self.config_entry, data={
-                CONF_COMPANIES: user_input[CONF_COMPANIES],
-                CONF_PRODUCTS: self.config_entry.data.get(CONF_PRODUCTS, [])
-            })
-            return await self.async_step_select_products()
-
-        companies = await self.hass.async_add_executor_job(fetch_companies)
-
-        schema = vol.Schema({
-            vol.Required(CONF_COMPANIES, default=self.config_entry.data.get(CONF_COMPANIES, [])): selector.SelectSelector(
-                selector.SelectSelectorConfig(
-                    options=list(companies.keys()) if companies else [],
-                    multiple=True,
-                    mode=selector.SelectSelectorMode.LIST,
-                )
-            ),
-        })
-
-        return self.async_show_form(step_id="select_companies", data_schema=schema)
-
-    async def async_step_select_products(self, user_input=None):
-        """Trin 2: Opdater valgte brændstofprodukter."""
-        _LOGGER.info("Reconfiguration: Trin 2 - Vælg brændstofprodukter")
-
-        if user_input is not None:
-            _LOGGER.debug("Opdaterede produkter: %s", user_input[CONF_PRODUCTS])
-            updated_data = {
-                CONF_COMPANIES: self.config_entry.data[CONF_COMPANIES],
-                CONF_PRODUCTS: [PRODUCT_NAME_MAP[name] for name in user_input[CONF_PRODUCTS]]
-            }
-            self.hass.config_entries.async_update_entry(self.config_entry, data=updated_data)
-
-            # Hent aktive enheder fra den nye konfiguration
-            active_entity_ids = set()
-            for company in updated_data[CONF_COMPANIES]:
-                for product in updated_data[CONF_PRODUCTS]:
-                    active_entity_ids.add(f"{self.config_entry.entry_id}_{company}_{product}")
-
-            # **Genindlæs integrationen for at anvende ændringer**
-            await self.hass.config_entries.async_reload(self.config_entry.entry_id)
-
-            # **Fjern gamle sensorer og enheder efter genindlæsning**
-            await remove_unused_entities_and_devices(self.hass, self.config_entry, active_entity_ids)
-
-            return self.async_create_entry(title="", data={})
-
-        schema = vol.Schema({
-            vol.Required(CONF_PRODUCTS, default=[PRODUCTS[p] for p in self.config_entry.data.get(CONF_PRODUCTS, [])]): selector.SelectSelector(
-                selector.SelectSelectorConfig(
-                    options=list(PRODUCT_NAME_MAP.keys()),
-                    multiple=True,
-                    mode=selector.SelectSelectorMode.LIST,
-                )
-            ),
-        })
-
-        return self.async_show_form(step_id="select_products", data_schema=schema)
+        return self.async_show_form(step_id="price_types", data_schema=schema)
